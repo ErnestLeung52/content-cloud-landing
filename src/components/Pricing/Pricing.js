efd66da7a97bf106398d31f2093e0993f83635d6
Ernest Leung <ernestleung52@gmail.com>
2023-05-30 11:30:15 -0700

    updated pricing

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,85 +1,85 @@
 import React from 'react';
 import { Button } from '../../globalStyles';
-import { AiFillThunderbolt } from 'react-icons/ai';
-import { GiCrystalBars } from 'react-icons/gi';
-import { GiCutDiamond, GiRock } from 'react-icons/gi';
-import { GiFloatingCrystal } from 'react-icons/gi';
+// import { AiFillThunderbolt } from 'react-icons/ai';
+import { GiPostStamp } from 'react-icons/gi';
+import { GiPhotoCamera, GiVideoCamera } from 'react-icons/gi';
+// import { GiFloatingCrystal } from 'react-icons/gi';
 import { IconContext } from 'react-icons/lib';
 import {
-  PricingSection,
-  PricingWrapper,
-  PricingHeading,
-  PricingContainer,
-  PricingCard,
-  PricingCardInfo,
-  PricingCardIcon,
-  PricingCardPlan,
-  PricingCardCost,
-  PricingCardLength,
-  PricingCardFeatures,
-  PricingCardFeature
+	PricingSection,
+	PricingWrapper,
+	PricingHeading,
+	PricingContainer,
+	PricingCard,
+	PricingCardInfo,
+	PricingCardIcon,
+	PricingCardPlan,
+	PricingCardCost,
+	PricingCardLength,
+	PricingCardFeatures,
+	PricingCardFeature,
 } from './Pricing.elements';
 
- const Pricing = () => {
-  return (
-    <IconContext.Provider value={{ color: '#a9b3c1', size: 64 }}>
-      <PricingSection>
-        <PricingWrapper>
-          <PricingHeading>Our Services</PricingHeading>
-          <PricingContainer>
-            <PricingCard to='/sign-up'>
-              <PricingCardInfo>
-                <PricingCardIcon>
-                  <GiRock />
-                </PricingCardIcon>
-                <PricingCardPlan>Starter Pack</PricingCardPlan>
-                <PricingCardCost>$99.99</PricingCardCost>
-                <PricingCardLength>per month</PricingCardLength>
-                <PricingCardFeatures>
-                  <PricingCardFeature>100 New Users</PricingCardFeature>
-                  <PricingCardFeature>$10,000 Budget</PricingCardFeature>
-                  <PricingCardFeature>Retargeting analytics</PricingCardFeature>
-                </PricingCardFeatures>
-                <Button primary>Choose Plan</Button>
-              </PricingCardInfo>
-            </PricingCard>
-            <PricingCard to='/sign-up'>
-              <PricingCardInfo>
-                <PricingCardIcon>
-                  <GiCrystalBars />
-                </PricingCardIcon>
-                <PricingCardPlan>Gold Rush</PricingCardPlan>
-                <PricingCardCost>$299.99</PricingCardCost>
-                <PricingCardLength>per month</PricingCardLength>
-                <PricingCardFeatures>
-                  <PricingCardFeature>1000 New Users</PricingCardFeature>
-                  <PricingCardFeature>$50,000 Budget</PricingCardFeature>
-                  <PricingCardFeature>Lead Gen Analytics</PricingCardFeature>
-                </PricingCardFeatures>
-                <Button primary>Choose Plan</Button>
-              </PricingCardInfo>
-            </PricingCard>
-            <PricingCard to='/sign-up'>
-              <PricingCardInfo>
-                <PricingCardIcon>
-                  <GiCutDiamond />
-                </PricingCardIcon>
-                <PricingCardPlan>Diamond Kings</PricingCardPlan>
-                <PricingCardCost>$999.99</PricingCardCost>
-                <PricingCardLength>per month</PricingCardLength>
-                <PricingCardFeatures>
-                  <PricingCardFeature>Unlimited Users</PricingCardFeature>
-                  <PricingCardFeature>Unlimited Budget</PricingCardFeature>
-                  <PricingCardFeature>24/7 Support</PricingCardFeature>
-                </PricingCardFeatures>
-                <Button primary>Choose Plan</Button>
-              </PricingCardInfo>
-            </PricingCard>
-          </PricingContainer>
-        </PricingWrapper>
-      </PricingSection>
-    </IconContext.Provider>
-  );
-}
+const Pricing = () => {
+	return (
+		<IconContext.Provider value={{ color: '#a9b3c1', size: 64 }}>
+			<PricingSection>
+				<PricingWrapper>
+					<PricingHeading>Our Services</PricingHeading>
+					<PricingContainer>
+						<PricingCard to='/sign-up'>
+							<PricingCardInfo>
+								<PricingCardIcon>
+									<GiVideoCamera />
+								</PricingCardIcon>
+								<PricingCardPlan>Video Edit</PricingCardPlan>
+								<PricingCardCost>$60</PricingCardCost>
+								<PricingCardLength>per 30 minutes</PricingCardLength>
+								<PricingCardFeatures>
+									<PricingCardFeature>Complete in 3-6 days</PricingCardFeature>
+									<PricingCardFeature>Output Format: MP4, MOV, AVI</PricingCardFeature>
+									<PricingCardFeature>24/7 Support</PricingCardFeature>
+								</PricingCardFeatures>
+								<Button primary>Purchase</Button>
+							</PricingCardInfo>
+						</PricingCard>
+						<PricingCard to='/sign-up'>
+							<PricingCardInfo>
+								<PricingCardIcon>
+									<GiPostStamp />
+								</PricingCardIcon>
+								<PricingCardPlan>Photoshop</PricingCardPlan>
+								<PricingCardCost>$15</PricingCardCost>
+								<PricingCardLength>per photo</PricingCardLength>
+								<PricingCardFeatures>
+									<PricingCardFeature>Complete in 1-2 days</PricingCardFeature>
+									<PricingCardFeature>Output Format: JPG, PNG, RAW</PricingCardFeature>
+									<PricingCardFeature>24/7 Support</PricingCardFeature>
+								</PricingCardFeatures>
+								<Button primary>Purchase</Button>
+							</PricingCardInfo>
+						</PricingCard>
+						<PricingCard to='/sign-up'>
+							<PricingCardInfo>
+								<PricingCardIcon>
+									<GiPhotoCamera />
+								</PricingCardIcon>
+								<PricingCardPlan>Photography</PricingCardPlan>
+								<PricingCardCost>$30</PricingCardCost>
+								<PricingCardLength>per hour</PricingCardLength>
+								<PricingCardFeatures>
+									<PricingCardFeature>Email for appointment</PricingCardFeature>
+									<PricingCardFeature>Output Format: JPG, PNG, RAW</PricingCardFeature>
+									<PricingCardFeature>24/7 Support</PricingCardFeature>
+								</PricingCardFeatures>
+								<Button primary>Purchase</Button>
+							</PricingCardInfo>
+						</PricingCard>
+					</PricingContainer>
+				</PricingWrapper>
+			</PricingSection>
+		</IconContext.Provider>
+	);
+};
 
-export default Pricing
+export default Pricing;
