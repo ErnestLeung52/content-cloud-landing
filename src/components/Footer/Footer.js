64f359f453ba99e2d380a79cd6350c93ebb584da
Ernest Leung <ernestleung52@gmail.com>
2023-05-30 11:29:07 -0700

    updated footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,50 +1,41 @@
 import React from 'react';
 import { Button } from '../../globalStyles';
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import {
-  FaFacebook,
-  FaInstagram,
-  FaYoutube,
-  FaTwitter,
-  FaLinkedin
-} from 'react-icons/fa';
-import {
-  FooterContainer,
-  FooterSubscription,
-  FooterSubText,
-  FooterSubHeading,
-  Form,
-  FormInput,
-  FooterLinksContainer,
-  FooterLinksWrapper,
-  FooterLinkItems,
-  FooterLinkTitle,
-  FooterLink,
-  SocialMedia,
-  SocialMediaWrap,
-  SocialLogo,
-  SocialIcon,
-  WebsiteRights,
-  SocialIcons,
-  SocialIconLink
+	FooterContainer,
+	FooterSubscription,
+	FooterSubText,
+	FooterSubHeading,
+	Form,
+	FormInput,
+	// FooterLinksContainer,
+	// FooterLinksWrapper,
+	// FooterLinkItems,
+	// FooterLinkTitle,
+	// FooterLink,
+	SocialMedia,
+	SocialMediaWrap,
+	SocialLogo,
+	SocialIcon,
+	WebsiteRights,
+	SocialIcons,
+	SocialIconLink,
 } from './Footer.elements';
 
 function Footer() {
+	const date = new Date();
 
-  const date = new Date();
-
-  return (
-    <FooterContainer>
-      <FooterSubscription>
-        <FooterSubHeading>
-          Join our exclusive membership to receive the latest news and trends
-        </FooterSubHeading>
-        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
-        <Form>
-          <FormInput name='email' type='email' placeholder='Your Email' />
-          <Button fontBig>Subscribe</Button>
-        </Form>
-      </FooterSubscription>
-      <FooterLinksContainer>
+	return (
+		<FooterContainer>
+			<FooterSubscription>
+				<FooterSubHeading>Join our exclusive membership to receive the latest news and trends</FooterSubHeading>
+				<FooterSubText>You can unsubscribe at any time.</FooterSubText>
+				<Form>
+					<FormInput name='email' type='email' placeholder='Your Email' />
+					<Button fontBig>Subscribe</Button>
+				</Form>
+			</FooterSubscription>
+			{/* <FooterLinksContainer>
         <FooterLinksWrapper>
           <FooterLinkItems>
             <FooterLinkTitle>About Us</FooterLinkTitle>
@@ -78,35 +69,35 @@ function Footer() {
             <FooterLink to='/'>Twitter</FooterLink>
           </FooterLinkItems>
         </FooterLinksWrapper>
-      </FooterLinksContainer>
-      <SocialMedia>
-        <SocialMediaWrap>
-          <SocialLogo to='/'>
-            <SocialIcon />
-            ULTRA
-          </SocialLogo>
-          <WebsiteRights>ULTRA © {date.getFullYear()} </WebsiteRights>
-          <SocialIcons>
-            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
-              <FaFacebook />
-            </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
-              <FaInstagram />
-            </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label='YouTube' >
-              <FaYoutube />
-            </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
-              <FaTwitter />
-            </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
-              <FaLinkedin />
-            </SocialIconLink>
-          </SocialIcons>
-        </SocialMediaWrap>
-      </SocialMedia>
-    </FooterContainer>
-  );
+      </FooterLinksContainer> */}
+			<SocialMedia>
+				<SocialMediaWrap>
+					<SocialLogo to='/'>
+						<SocialIcon />
+						Pixel Perfect Productions
+					</SocialLogo>
+					<WebsiteRights>Pixel Perfect Productions © {date.getFullYear()} </WebsiteRights>
+					<SocialIcons>
+						<SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+							<FaFacebook />
+						</SocialIconLink>
+						<SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+							<FaInstagram />
+						</SocialIconLink>
+						<SocialIconLink href='/' target='_blank' aria-label='YouTube'>
+							<FaYoutube />
+						</SocialIconLink>
+						<SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+							<FaTwitter />
+						</SocialIconLink>
+						<SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+							<FaLinkedin />
+						</SocialIconLink>
+					</SocialIcons>
+				</SocialMediaWrap>
+			</SocialMedia>
+		</FooterContainer>
+	);
 }
 
 export default Footer;
